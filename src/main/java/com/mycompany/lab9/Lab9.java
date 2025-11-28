a2b5307f22223d0be7af716b5097f6fea1b643fe
asmaa580 <asmaaadel918273@gmail.com>
2025-11-28 14:28:34 +0200

    create main

---
+++ src/main/java/com/mycompany/lab9/Lab9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab9;
+
+/**
+ *
+ * @author USER
+ */
+public class Lab9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
