ee78244f470381ebb3935eac4d05595fbed7f121
malak12111 <malakayman5@gmail.com>
2025-11-28 18:40:32 +0200

    update

--- src/main/java/com/mycompany/lab9/Lab9.java
+++ src/main/java/com/mycompany/lab9/Lab9.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab9;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USER
@@ -12,9 +14,15 @@ public class Lab9 {
 
     public static void main(String[] args) {
           // 1. Read arguments
-        String filePath = args[0]; // CSV file
-        int mode = Integer.parseInt(args[1]); // 0, 3, or 27
+        Scanner sc = new Scanner(System.in);
 
+        // Ask user for file path
+        System.out.print("Enter CSV file path: ");
+        String filePath = sc.nextLine();
+
+        // Ask user for mode
+        System.out.print("Enter mode (0, 3, or 27): ");
+        int mode = sc.nextInt();
         // 2. Load board
         int[][] board = CSVLoader.loadBoard(filePath);
 
@@ -22,12 +30,12 @@ public class Lab9 {
         if (mode == 0) {
             SequentialChecker.run(board);
         } 
-        else if (mode == 3) {
+       /* else if (mode == 3) {
             Mode3Checker.run(board);
         } 
         else if (mode == 27) {
             Mode27Checker.run(board);
-        } 
+        } */
         else {
             System.out.println("Invalid mode. Choose 0, 3, or 27.");
         }
