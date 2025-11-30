a8ccf64a28868ba97d4a1a003fb32f6640e882df
asmaa580 <asmaaadel918273@gmail.com>
2025-11-30 18:11:58 +0200

    getting input from git bash

--- src/main/java/com/mycompany/lab9/SequentialChecker.java
+++ src/main/java/com/mycompany/lab9/SequentialChecker.java
@@ -5,25 +5,38 @@ import java.util.List;
 
 public class SequentialChecker {
      public static void run(int[][] board) {
-        List<String> errors = new ArrayList<>();
+        List<String> rerrors = new ArrayList<>();
+         List<String> cerrors = new ArrayList<>();
+          List<String> berrors = new ArrayList<>();
 
-        checkRows(board, errors);
-        checkCols(board, errors);
-        checkBoxes(board, errors);
+        checkRows(board, rerrors);
+        checkCols(board, cerrors);
+        checkBoxes(board, berrors);
 
-        if (errors.isEmpty()) {
+        if (rerrors.isEmpty()&&cerrors.isEmpty()&&berrors.isEmpty()) {
             System.out.println("VALID");
         } else {
             System.out.println("INVALID");
-            for (String e : errors)
+            for (String e : rerrors)
                 System.out.println(e);
+            System.out.println("---------------------");
+            
+            for (String e : cerrors)
+                System.out.println(e);
+            System.out.println("---------------------");
+            
+            for (String e : berrors)
+                System.out.println(e);
+            
         }
     }
-     public static void checkOneRow(int[][] board, int r, List<String> errors) {
+     /*public static void checkOneRow(int[][] board, int r, List<String> errors) {
         int[] count = new int[10];
+
 
         for (int c = 0; c < 9; c++) 
             count[board[r][c]]++;
+
 
         for (int num = 1; num <= 9; num++) 
             if (count[num] > 1) 
@@ -60,6 +73,62 @@ public class SequentialChecker {
                 errors.add("BOX " + (b + 1) + " duplicate " + num);
             
      }
+  */
+     public static void checkOneRow(int[][] board, int r, List<String> errors) {
+    // map number -> list of positions
+    List<Integer>[] positions = new ArrayList[10];
+    for (int i = 1; i <= 9; i++) positions[i] = new ArrayList<>();
+
+    for (int c = 0; c < 9; c++) {
+        int val = board[r][c];
+        positions[val].add(c + 1); // store column index (1-based)
+    }
+
+    for (int num = 1; num <= 9; num++) {
+        if (positions[num].size() > 1) {
+            errors.add("ROW " + (r + 1) + ", #" + num + ", " + positions[num]);
+        }
+    }
+}
+
+     public static void checkOneCol(int[][] board, int c, List<String> errors) {
+    List<Integer>[] positions = new ArrayList[10];
+    for (int i = 1; i <= 9; i++) positions[i] = new ArrayList<>();
+
+    for (int r = 0; r < 9; r++) {
+        int val = board[r][c];
+        positions[val].add(r + 1); // store row index (1-based)
+    }
+
+    for (int num = 1; num <= 9; num++) {
+        if (positions[num].size() > 1) {
+            errors.add("COL " + (c + 1) + ", #" + num + ", " + positions[num]);
+        }
+    }
+}
+
+     public static void checkOneBox(int[][] board, int b, List<String> errors) {
+    List<Integer>[] positions = new ArrayList[10];
+    for (int i = 1; i <= 9; i++) positions[i] = new ArrayList<>();
+
+    int br = b / 3;
+    int bc = b % 3;
+
+    int index = 1;
+    for (int r = br * 3; r < br * 3 + 3; r++) {
+        for (int c = bc * 3; c < bc * 3 + 3; c++) {
+            int val = board[r][c];
+            positions[val].add(index); // store position inside box (1–9)
+            index++;
+        }
+    }
+
+    for (int num = 1; num <= 9; num++) {
+        if (positions[num].size() > 1) {
+            errors.add("BOX " + (b + 1) + ", #" + num + ", " + positions[num]);
+        }
+    }
+}
 
      
     public static void checkRows(int[][] board, List<String> errors) {
@@ -69,16 +138,25 @@ public class SequentialChecker {
     
     }
 
-   
-    public static void checkCols(int[][] board,List<String> errors) {
+
+    // -----------------------------
+    // CHECK COLUMNS
+    // -----------------------------
+    public static void checkCols(int[][] board, List<String> errors) {
+
 
          for (int c = 0; c < 9; c++) 
         checkOneCol(board, c, errors);
     
     }
 
-   
+
+    // -----------------------------
+    // CHECK 3×3 BOXES
+    // -----------------------------
+
     public static void checkBoxes(int[][] board,List<String> errors) {
+
 
       for (int b = 0; b < 9; b++) 
         checkOneBox(board, b, errors);
